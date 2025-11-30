43edb5bed26b0b8123b6d090993fe96333141b0a
ayush348rtg <X.xxxxvxxvxxx@gmail.com>
2025-11-30 10:54:44 +0530

    Add files via upload

---
+++ app/page-7/e_thumbnail.js
@@ -0,0 +1,15 @@
+function name_p_n_11() {
+  document.querySelectorAll(".content_section").forEach(sec => sec.classList.remove("active"));
+  document.getElementById("class11_physics_chap").classList.add("active");
+  setActiveIcon("class11_physics_chap");
+}
+function name_c_n_11() {
+  document.querySelectorAll(".content_section").forEach(sec => sec.classList.remove("active"));
+  document.getElementById("class11_chemistry_chap").classList.add("active");
+  setActiveIcon("class11_chemistry_chap");
+}
+function name_b_n_11() {
+  document.querySelectorAll(".content_section").forEach(sec => sec.classList.remove("active"));
+  document.getElementById("class11_biology_chap").classList.add("active");
+  setActiveIcon("class11_biology_chap");
+}
